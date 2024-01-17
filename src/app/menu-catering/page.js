798bd45c2f10b12560277fc639d352dc4097ab93
Sean Conrad <seanconrad123@gmail.com>
2024-01-16 22:30:31 -0600

    pushing up latest

--- src/app/menu-catering/page.js
+++ src/app/menu-catering/page.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import styles from "./menu-catering.module.css";
 
 const page = () => {
-	return (
-		<div>page</div>
-	)
-}
+  return (
+    <iframe
+      className={styles.iframe}
+      src="https://places.singleplatform.com/woodys-47/menu_widget?api_key=ke09z8icq4xu8uiiccighy1bw&base_font_size=18px&hide_cover_photo=false&hide_disclaimer=true&item_desc_font_color=%23514b4b&item_price_font_color=%23514b4b&item_title_font_color=%23514b4b&menu_desc_background_color=%23ffffff&menu_desc_font_color=%23514b4b&primary_background_color=%23ffffff&primary_font_color=%23514b4b&primary_font_family=Open%20Sans&section_desc_background_color=%23ffffff&section_desc_font_color=%23514b4b&section_title_background_color=%23ffffff&section_title_font_color=%23514b4b&stylesheet=https%3A//fonts.googleapis.com/css%3Ffamily%3DOpen+Sans&widget_background_color=rgba%280%2C%200%2C%200%2C%200%29"
+    />
+  );
+};
 
-export default page
+export default page;
