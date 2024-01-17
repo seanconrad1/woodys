a132fa7c79bc38bd76e09fd76b0df09f3585e7d1
Sean Conrad <seanconrad123@gmail.com>
2024-01-17 16:16:27 -0600

    this is coming together

--- src/app/components/SideNav/SideNav.js
+++ src/app/components/SideNav/SideNav.js
@@ -1,42 +1,128 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "./SideNav.module.css";
 import SocialsContainer from "../SocialsContainer/SocialsContainer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useWindowSize } from "@react-hook/window-size";
+import { faUtensils } from "@fortawesome/free-solid-svg-icons";
+
+const orderPickupLink = "https://ordering.chownow.com/order/6077/locations?add_cn_ordering_class=true"
+const orderDeliveryLink = "https://www.ubereats.com/store/woodys-famous-salads-south-tampa/bv6aTsx2Rm6xnWPw13gSbg?diningMode=DELIVERY&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMjQ1OTIlMjBTJTIwVHdpbmxlYWYlMjBEciUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMmhlcmUlM0FhZiUzQXN0cmVldHNlY3Rpb24lM0FWNG1RM2tmbTl0RmplSGNnMGlpR2hDJTNBQ2djSUJDQzUtdk1rRUFFYUJEUTFPVEklMjIlMkMlMjJyZWZlcmVuY2VUeXBlJTIyJTNBJTIyaGVyZV9wbGFjZXMlMjIlMkMlMjJsYXRpdHVkZSUyMiUzQTMzLjI2Njg2JTJDJTIybG9uZ2l0dWRlJTIyJTNBLTExMS43NzIxNCU3RA%3D%3D"
 
 const SideNav = () => {
-  return (
-    <div className={styles.sideNavContainer}>
-      <nav className={styles.sideNav}>
-        <ul className={styles.sideNavMenu}>
-          <li className={`${styles.listItem} ${styles.location}`}>
-            <div>TAMPA</div>
-          </li>
-          <li className={styles.listItem}>
-            <a href="/" className={styles.listItemLink}>
-              Home
-            </a>
-          </li>
-          <li className={styles.listItem}>
-            <a href="/menu-catering" className={styles.listItemLink}>
-						Menu &amp; Catering
-            </a>
-          </li>
-          <li className={styles.listItem}>
-            <a href="/weekly-soups" className={styles.listItemLink}>
-              Weekly Soups
-            </a>
-          </li>
+  const [windowWidth] = useWindowSize();
 
-          <li className={styles.listItem}>
-            <a href="/gallery" className={styles.listItemLink}>
-              Gallery
-            </a>
-          </li>
-        </ul>
-      </nav>
+  const [isOpen, setIsOpen] = useState(false);
+  const [height, setHeight] = useState("0px");
 
-      <SocialsContainer size={"xl"} color={"black"} />
-    </div>
-  );
+  const contentRef = useRef(null);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    setHeight(isOpen ? `${contentRef.current.scrollHeight}px` : "0px");
+  }, [isOpen]);
+
+  // Mobile
+  if (windowWidth < 768) {
+    return (
+      <>
+        <div className={styles.barContainer}>
+          <button className={styles.button} onClick={toggleNav}>
+            <FontAwesomeIcon
+              className={styles.faIcon}
+              color={"black"}
+              size={"xl"}
+              icon={faUtensils}
+            />
+            <div className={styles.label}>{"Menu"}</div>
+          </button>
+
+          <a href={orderPickupLink} target="_blank" className={styles.button}>
+            ORDER PICKUP
+          </a>
+
+          <a href={orderDeliveryLink} target="_blank" className={styles.button}>
+            ORDER DELIVERY
+          </a>
+        </div>
+        <div className={styles.sideNavContainer}>
+          <nav
+            ref={contentRef}
+            style={{ height }}
+            className={`${styles.sideNav} `}
+          >
+            <ul className={styles.sideNavMenu}>
+              <li className={`${styles.listItem} ${styles.location}`}>
+                <div>TAMPA</div>
+              </li>
+              <li className={styles.listItem}>
+                <a href="/" className={styles.listItemLink}>
+                  Home
+                </a>
+              </li>
+              <li className={styles.listItem}>
+                <a href="/menu-catering" className={styles.listItemLink}>
+                  Menu &amp; Catering
+                </a>
+              </li>
+              <li className={styles.listItem}>
+                <a href="/weekly-soups" className={styles.listItemLink}>
+                  Weekly Soups
+                </a>
+              </li>
+
+              <li className={styles.listItem}>
+                <a href="/gallery" className={styles.listItemLink}>
+                  Gallery
+                </a>
+              </li>
+
+              <li>
+                <SocialsContainer size={"xl"} color={"black"} />
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <div className={`${styles.sideNavContainer}`}>
+        <nav className={`${styles.sideNav}`}>
+          <ul className={styles.sideNavMenu}>
+            <li className={`${styles.listItem} ${styles.location}`}>
+              <div>TAMPA</div>
+            </li>
+            <li className={styles.listItem}>
+              <a href="/" className={styles.listItemLink}>
+                Home
+              </a>
+            </li>
+            <li className={styles.listItem}>
+              <a href="/menu-catering" className={styles.listItemLink}>
+                Menu &amp; Catering
+              </a>
+            </li>
+            <li className={styles.listItem}>
+              <a href="/weekly-soups" className={styles.listItemLink}>
+                Weekly Soups
+              </a>
+            </li>
+
+            <li className={styles.listItem}>
+              <a href="/gallery" className={styles.listItemLink}>
+                Gallery
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <SocialsContainer size={"xl"} color={"black"} />
+      </div>
+    );
+  }
 };
 
 export default SideNav;
--- src/app/components/TopBar/TopBar.js
+++ src/app/components/TopBar/TopBar.js
@@ -1,15 +1,17 @@
+"use client";
 import React from "react";
 import styles from "./TopBar.module.css";
-import brandImage from "../../assets/brand.png";
 import brandWhite from "../../assets/woodys_white.jpeg";
-import barBg from "../../assets/bar_bg.jpeg";
+import brandText from "../../assets/brand2.jpeg";
 import Image from "next/image";
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
+import { useWindowSize } from "@react-hook/window-size";
 
-const avenirFont = localFont({ src: '../../assets/fonts/avenir_next.woff2' })
+const avenirFont = localFont({ src: "../../assets/fonts/avenir_next.woff2" });
 
 const TopBar = () => {
-
+  const [windowWidth] = useWindowSize();
+  console.log(windowWidth < 1100 ? brandText : brandWhite);
   const pickupLink =
     "https://ordering.chownow.com/order/6077/locations?add_cn_ordering_class=true";
   const deliveryLink =
@@ -20,30 +22,43 @@ const TopBar = () => {
       <a href="/" className={styles.brandImageLink}>
         <Image
           width={"300px"}
-          height={"300px"}
-          src={brandWhite}
+          height={"0px"}
+          src={windowWidth < 1100 ? brandText : brandWhite}
           alt="Woodys Restaurant"
           className={styles.brandImage}
         ></Image>
       </a>
-      <div className={`${styles.bars} ${avenirFont.className}`}>
-        <div className={`${styles.bar} ${styles.bar1}`}>
-          <a href="/about" className={styles.bar1Item}>About Us</a>
-        </div>
-        <div className={`${styles.bar} ${styles.bar2}`}>
-          <div className={styles.bar2Item}>
-            1722 S DALE MABRY HWY TAMPA, FL 33629 813.254.2806
-          </div>
-          <div className={styles.orderButtons}>
-            <a target="_blank" href={pickupLink} className={styles.orderNowLink}>
-              ORDER PICKUP
-            </a>
-            <a target="_blank" href={deliveryLink} className={styles.orderNowLink}>
-              ORDER DELIVERY
+
+      {windowWidth < 768 ? null : (
+        <div className={`${styles.bars} ${avenirFont.className}`}>
+          <div className={`${styles.bar} ${styles.bar1}`}>
+            <a href="/about" className={styles.bar1Item}>
+              About Us
             </a>
           </div>
+          <div className={`${styles.bar} ${styles.bar2}`}>
+            <div className={styles.bar2Item}>
+              1722 S DALE MABRY HWY TAMPA, FL 33629 813.254.2806
+            </div>
+            <div className={styles.orderButtons}>
+              <a
+                target="_blank"
+                href={pickupLink}
+                className={styles.orderNowLink}
+              >
+                ORDER PICKUP
+              </a>
+              <a
+                target="_blank"
+                href={deliveryLink}
+                className={styles.orderNowLink}
+              >
+                ORDER DELIVERY
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
