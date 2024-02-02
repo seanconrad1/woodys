a4ff2a7b2a15c58bf1f446cd3bae5501b87729e8
Sean M Conrad <sean_m_conrad@mckinsey.com>
2024-02-02 11:57:09 -0600

    fixed styling

--- src/app/components/MenuItem/MenuItem.js
+++ src/app/components/MenuItem/MenuItem.js
@@ -3,28 +3,34 @@ import styles from "./MenuItem.module.css";
 import { truncateText } from "../../../utils/utils";
 
 const MenuItem = ({ name, price, description, image }) => {
-
   return (
     <div className={styles.sectionContainer}>
       <div className={styles.menuItem}>
-        <div id="text-container" className={styles.leftSide}>
-          <div className={styles.menuItemName}>{name}</div>
-          <div className={styles.price}>{`$${price}.00`}</div>
-          <div className={styles.description} id="description">
-            {truncateText(description)}
+        <div className={styles.menuAndDesc}>
+          <div id="text-container" className={styles.leftSide}>
+            <div className={styles.menuItemName}>{name}</div>
+            <div className={styles.price}>{`$${price}.00`}</div>
+            {/* <div className={styles.description} id="description">
+            {description}
+          </div> */}
           </div>
+
+          {image && (
+            <div className={styles.menuItemImgContainer}>
+              <Image
+                width={250}
+                height={250}
+                className={styles.menuItemImg}
+                src={image}
+                alt={name}
+              />
+            </div>
+          )}
         </div>
-        {image && (
-          <div className={styles.menuItemImgContainer}>
-            <Image
-              width={250}
-              height={250}
-              className={styles.menuItemImg}
-              src={image}
-              alt={name}
-            />
-          </div>
-        )}
+
+        <div className={styles.description} id="description">
+          {description}
+        </div>
       </div>
     </div>
   );
