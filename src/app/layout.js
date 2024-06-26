8469a2c6f4f099c2e8c4963432df78eec7638ec9
Sean M Conrad <sean_m_conrad@mckinsey.com>
2024-06-26 19:10:03 -0400

    updated icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,16 +15,13 @@ const playFairFont = Playfair_Display({
   display: "swap",
 });
 
-
-
 export const metadata = {
-  metadataBase: new URL('https://www.woodysofsouthtampa.com'),
+  metadataBase: new URL("https://www.woodysofsouthtampa.com"),
 
   icons: {
-    icon: './assets/woodys_white.jpeg',
-    shortcut: './assets/woodys_white.jpeg',
-    apple: './assets/woodys_white.jpeg',
-
+    icon: "./assets/woodys_white.jpeg",
+    shortcut: "./assets/woodys_white.jpeg",
+    apple: "./assets/woodys_white.jpeg",
   },
   robots: {
     index: false,
@@ -34,38 +31,54 @@ export const metadata = {
       index: true,
       follow: false,
       noimageindex: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
   openGraph: {
-    title: 'Woodys Famous Greek Salads',
-    description: 'The React Framework for the Web',
-    url: 'https://www.woodysofsouthtampa.com',
-    siteName: 'Next.js',
+    title: "Woodys Famous Greek Salads",
+    description:
+      "Serving the Tampa area since 1973, Woody’s Famous Greek Salads is a family-owned and operated restaurant that offers a variety of delicious dishes.",
+    url: "https://www.woodysofsouthtampa.com",
+    siteName: "Next.js",
+    keywords: ["Greek", "Salads", "Tampa", "Restaurant", "Woody's"],
     images: [
       {
-        url: 'https://nextjs.org/og.png', // Must be an absolute URL
+        url: "https://nextjs.org/og.png", // Must be an absolute URL
         width: 800,
         height: 600,
       },
       {
-        url: 'https://nextjs.org/og-alt.png', // Must be an absolute URL
+        url: "https://nextjs.org/og-alt.png", // Must be an absolute URL
         width: 1800,
         height: 1600,
-        alt: 'My custom alt',
+        alt: "My custom alt",
       },
     ],
-    locale: 'en_US',
-    type: 'website',
+    locale: "en_US",
+    type: "website",
   },
-}
-
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body className={`${inter.className} ${playFairFont.className}`}>
         <TopBar />
         {children}
