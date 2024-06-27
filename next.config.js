dfd8026c7a5e61081484e19640ed25b26c0fe3d8
Sean M Conrad <sean_m_conrad@mckinsey.com>
2024-06-26 22:58:46 -0400

    cleaning up some stuff

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID || "",
-    CONTENTFUL_DELIVERY_API_KEY: process.env.CONTENTFUL_DELIVERY_API_KEY || "",
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_DELIVERY_API_KEY: process.env.CONTENTFUL_DELIVERY_API_KEY,
   },
   images: {
     remotePatterns: [
