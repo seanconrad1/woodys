491f4d3079eda5415ff18af4366f2552688271a9
Sean M Conrad <sean_m_conrad@mckinsey.com>
2024-07-01 09:40:44 -0400

    shuffled the images in carousel

--- src/app/components/ImageCrossfader/ImageCrossfader.js
+++ src/app/components/ImageCrossfader/ImageCrossfader.js
@@ -63,6 +63,25 @@ const ImageCrossfader = () => {
     return () => clearInterval(timer);
   }, [imagesLength]);
 
+  const shuffledArray = (array) => {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+  };
+
+  shuffledArray(imagesArr);
+
   return (
     <div className={styles.crossfaderContainer}>
       {imagesArr.map((image, idx) => {
